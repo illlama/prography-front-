fd5b067ff9aa69c46e8bf096361fcb65ca20a946
illlama <beatlemaniakih@gmail.com>
2019-02-21 19:05:53 +0700

    resize and menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,42 +11,7 @@ $(document).ready(function() {
        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
     });
     
-    /* navigation scroll */
-    $('a[href*="#"]')
-      // Remove links that don't actually link to anything
-      .not('[href="#"]')
-      .not('[href="#0"]')
-      .click(function(event) {
-        // On-page links
-        if (
-          location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-          && 
-          location.hostname == this.hostname
-        ) {
-          // Figure out element to scroll to
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-          // Does a scroll target exist?
-          if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $('html, body').animate({
-              scrollTop: target.offset().top
-            }, 1000, function() {
-              // Callback after animation
-              // Must change focus!
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) { // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              };
-            });
-          }
-        }
-      });
+  
     
     /*---------------------*/
     /* Animations on scroll*/
@@ -66,5 +31,23 @@ $(document).ready(function() {
        $(this).parent().find('.project-card-descript').fadeToggle('1000'); 
     });
      
-
+       /* Mobile navigation */ 
+    $('.js--nav-icon').click(function() {
+       var nav = $('.js--main-nav');
+       var icon = $('.js--nav-icon ion-icon');
+        nav.slideToggle(200);
+         
+        if (icon.hasClass('js--nav-icon-close')){
+            icon.attr('name', 'menu');
+            icon.removeClass('js--nav-icon-close');
+            icon.addClass('js--nav-icon-menu');
+            
+            
+        }else{
+            icon.attr('name', 'close');
+            icon.removeClass('js--nav-icon-menu');
+            icon.addClass('js--nav-icon-close');  
+        }
+        
+    });
 });
